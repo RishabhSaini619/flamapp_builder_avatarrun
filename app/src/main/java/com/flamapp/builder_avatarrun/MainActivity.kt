fe5f58fff0ff0a28fb7969b836f1cc1720e2394e
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-04-29 14:18:06 +0530

    1.0

--- app/src/main/java/com/flamapp/builder_avatarrun/MainActivity.kt
+++ app/src/main/java/com/flamapp/builder_avatarrun/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.flamapp.builder_avatarrun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonFlam = findViewById<Button>(R.id.flam_button)
+        val buttonRPM = findViewById<Button>(R.id.rpm_button)
+        buttonFlam.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", "https://fe-avatar-system-next.vercel.app/")
+            startActivity(intent)
+        }
+        buttonRPM.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", "https://flam.readyplayer.me/avatar?frameApi")
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/flamapp/builder_avatarrun/WebViewActivity.kt
+++ app/src/main/java/com/flamapp/builder_avatarrun/WebViewActivity.kt
@@ -1,11 +1,36 @@
 package com.flamapp.builder_avatarrun
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
 
 class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        val webView = findViewById<WebView>(R.id.web_view_field)
+
+        val url = intent.getStringExtra("URL")
+        Log.i("URL",url.toString())
+        if (url != null) {
+
+            webView.apply {
+                loadUrl(url.toString())
+                settings.javaScriptEnabled = true
+                settings.allowFileAccess = true
+                settings.databaseEnabled = true
+                settings.allowContentAccess = true
+                settings.domStorageEnabled = true
+                settings.builtInZoomControls = false
+                settings.cacheMode = WebSettings.LOAD_DEFAULT
+                settings.javaScriptCanOpenWindowsAutomatically = true
+            }
+        }
+
     }
 }
